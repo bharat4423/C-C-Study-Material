cc143a65125d65b53137e96c8f2562469f184ae6
Bharat Bansode <94060912+bharat4423@users.noreply.github.com>
2023-10-31 12:41:32 +0530

    MyfirstCProg

---
+++ firstcprog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello world!\n");
+	return 0;
+}
+/*
+ * OutPut -->
+corporate@ACTS23:~/c> vi firstcprog.c
+corporate@ACTS23:~/c> gcc firstcprog.c
+corporate@ACTS23:~/c> ./a.out
+Hello world!
+*/
